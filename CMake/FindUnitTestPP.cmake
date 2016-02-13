6253ebe18b6536ffdaed9717e8eb02be17407389
Espen <espenb@hotmail.no>
2016-02-13 16:30:12 +0100

    Updated UnitTest++ CMakeFiles to work with Windows again + additional cleaning

--- CMake/FindUnitTestPP.cmake
+++ CMake/FindUnitTestPP.cmake
@@ -1,9 +1,33 @@
-FIND_PATH(UnitTestPP_INCLUDE_DIR UnitTest++.h
-   "/usr/include"
-   "/usr/local/include"
-	DOC "The directory where UnitTest++.h resides")
+# - Try to find UnitTest++
+#
+#
 
-FIND_LIBRARY(UnitTestPP_LIBRARY UnitTest++.h
-   "/usr/lib"
-   "/usr/local/lib"
-	DOC "The directory where UnitTest++.h resides")
+FIND_PATH (UnitTestPP_INCLUDE_DIR UnitTest++.h
+	/usr/include/unittestPP 
+	/usr/local/include/unittest++ 
+	/opt/local/include/unittest++
+	$ENV{UNITTESTXX_PATH}/src 
+	$ENV{UNITTESTXX_INCLUDE_PATH}
+)
+
+FIND_LIBRARY (UnitTestPP_LIBRARY NAMES UnitTest++ PATHS 
+	/usr/lib 
+	$ENV{UNITTESTXX_PATH} 
+	ENV{UNITTESTXX_LIBRARY_PATH}
+)
+
+SET (UNITTESTPP_FOUND FALSE)
+
+IF (UnitTestPP_INCLUDE_DIR AND UnitTestPP_LIBRARY)
+	SET (UNITTESTPP_FOUND TRUE)
+ENDIF (UnitTestPP_INCLUDE_DIR AND UnitTestPP_LIBRARY)
+
+IF (UNITTESTPP_FOUND)
+   IF (NOT UnitTestPP_FIND_QUIETLY)
+      MESSAGE(STATUS "Found UnitTest++: ${UnitTestPP_LIBRARY}")
+   ENDIF (NOT UnitTestPP_FIND_QUIETLY)
+ELSE (UNITTESTPP_FOUND)
+   IF (UnitTestPP_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "Could not find UnitTest++")
+   ENDIF (UnitTestPP_FIND_REQUIRED)
+ENDIF (UNITTESTPP_FOUND)
