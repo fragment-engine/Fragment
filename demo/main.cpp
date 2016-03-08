2876bc60b3f2527afa3b6255053872a8f8822460
Frank Lyder Bredland <fl@nyggen.com>
2016-03-08 16:51:59 +0100

    Got v8 to compile!

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,53 +1,72 @@
-#include <Fragment/Vector2.h>
-#include <Fragment/Window.h>
+// Copyright 2015 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-#include <SDL.h>
-#include <gl/glew.h>
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void _assertGlErrors(GLenum err, std::string file, int line)
-{
-   if( err != GL_NO_ERROR ) {
-      std::string error;
+#include "include/libplatform/libplatform.h"
+#include "include/v8.h"
 
-      switch(err) {
-         case GL_INVALID_OPERATION:              error="INVALID_OPERATION";      break;
-         case GL_INVALID_ENUM:                   error="INVALID_ENUM";           break;
-         case GL_INVALID_VALUE:                  error="INVALID_VALUE";          break;
-         case GL_OUT_OF_MEMORY:                  error="OUT_OF_MEMORY";          break;
-         case GL_INVALID_FRAMEBUFFER_OPERATION:  error="INVALID_FRAMEBUFFER_OPERATION";  break;
-      }
+using namespace v8;
 
-      std::cerr << line << "@" << file<< ": OpenGL error: " << error << " (" << err << ")" << std::endl;
+class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
+public:
+    virtual void* Allocate(size_t length) {
+       void* data = AllocateUninitialized(length);
+       return data == NULL ? data : memset(data, 0, length);
+    }
+    virtual void* AllocateUninitialized(size_t length) { return malloc(length); }
+    virtual void Free(void* data, size_t) { free(data); }
+};
+
+
+int main(int argc, char* argv[]) {
+   // Initialize V8.
+   V8::InitializeICU();
+   V8::InitializeExternalStartupData(argv[0]);
+   Platform* platform = platform::CreateDefaultPlatform();
+   V8::InitializePlatform(platform);
+   V8::Initialize();
+
+   // Create a new Isolate and make it the current one.
+   ArrayBufferAllocator allocator;
+   Isolate::CreateParams create_params;
+   create_params.array_buffer_allocator = &allocator;
+   Isolate* isolate = Isolate::New(create_params);
+   {
+      Isolate::Scope isolate_scope(isolate);
+
+      // Create a stack-allocated handle scope.
+      HandleScope handle_scope(isolate);
+
+      // Create a new context.
+      Local<Context> context = Context::New(isolate);
+
+      // Enter the context for compiling and running the hello world script.
+      Context::Scope context_scope(context);
+
+      // Create a string containing the JavaScript source code.
+      Local<String> source =
+              String::NewFromUtf8(isolate, "'Hello' + ', World!'",
+                                  NewStringType::kNormal).ToLocalChecked();
+
+      // Compile the source code.
+      Local<Script> script = Script::Compile(context, source).ToLocalChecked();
+
+      // Run the script to get the result.
+      Local<Value> result = script->Run(context).ToLocalChecked();
+
+      // Convert the result to an UTF8 string and print it.
+      String::Utf8Value utf8(result);
+      printf("%s\n", *utf8);
    }
-}
 
-#define assertGlErrors() _assertGlErrors(glGetError(), __FILE__, __LINE__)
-
-
-int main()
-{
-   Fragment::Window win(640, 480, "Fragment Demo");
-   win.init();
-
-   assertGlErrors();
-
-   SDL_Event event;
-   while (true) {
-
-      //assertGlErrors();
-
-      //Clear color buffer
-      glClear( GL_COLOR_BUFFER_BIT );
-
-      //Update screen
-      SDL_GL_SwapWindow( win.getWindow() );
-
-      if (SDL_PollEvent(&event)) {
-         if (event.type == SDL_QUIT)
-            break;
-      }
-   }
-
+   // Dispose the isolate and tear down V8.
+   isolate->Dispose();
+   V8::Dispose();
+   V8::ShutdownPlatform();
+   delete platform;
    return 0;
 }
